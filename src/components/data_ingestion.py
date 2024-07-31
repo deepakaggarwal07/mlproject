6d4ae0df9158280d9e89b68d26d503863d607e8d
Deepak Aggarwal <deepak.sandyjons@gmail.com>
2024-07-31 20:48:41 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ from dataclasses import dataclass
 sys.path.insert(0, '/Volumes/gyaniData/projects/web-applications/python/mlp1/src')
 from exception import CustomException
 from logger import logging
+from components.data_transformation import DataTransformation
+from components.data_transformation import DataTransformationConfirg
 
 @dataclass
 class DataIngestionConfirg:
@@ -46,5 +48,8 @@ class DataIngestion :
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data= obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
         
